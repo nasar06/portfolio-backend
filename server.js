475de020239cafe8decf84fd7ec9a08f60076382
nasar06 <nasar5847@gmail.com>
2025-08-19 09:29:31 +0600

    error solved

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
-.env
+
     console.log("✅ MongoDB connected");
   })
   .catch(err => console.error("Mongo error:", err));
